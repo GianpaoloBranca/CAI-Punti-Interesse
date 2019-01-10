d437195a3a2b81de6d2cf2c6cf5e7bf24e42ceac
Gianpaolo Branca <gianpaolo@protonmail.com>
2019-01-10 16:33:04 +0100

    add main model class 'PuntoInteresse'

--- punti_interesse/models.py
+++ punti_interesse/models.py
@@ -1,5 +1,47 @@
 from django.db import models
 
+class PuntoInteresse(models.Model):
+    coordinate = models.DecimalField(verbose_name='Coordinate', max_digits=9, decimal_places=6)
+    categoria = models.ForeignKey('TipoInteresse', verbose_name='Tipologia')
+    tipo = models.ForeignKey('InteresseSpecifico', verbose_name='Oggetto Specifico')
+
+    nome = models.CharField(verbose_name='Nome', max_length=128)
+    localita = models.CharField(verbose_name='Località', max_length=128)
+    valle = models.CharField(verbose_name='Valle', max_length=128)
+    qualita = models.ForeignKey('QualitaInteresse', verbose_name='Qualità interesse')
+    estensione = models.ForeignKey('EstensioneInteresse', verbose_name='Estensione Interesse')
+
+    valenza = models.CharField(verbose_name='Titolo della valenza', max_length=128)
+    visitabile = models.BooleanField(verbose_name='Visitabile')
+    visitabile2 = models.BooleanField(verbose_name='Visitabile per persone con ridotta capacità motoria o sensoriale')
+    periodo = models.CharField(verbose_name='Periodo di visita consigliato', max_length=256)
+    istituto = models.CharField(verbose_name='Istituto di tutela', max_length=128)
+
+    #foto copertina (max 1)
+    #foto accessorie (max 5)
+
+    descr_breve = models.TextField(verbose_name='Descrizione oggetto breve', max_length=256)
+    descr_estesa = models.TextField(verbose_name='Descrizione oggetto estesa', max_length=1024)
+    descr_sito = models.TextField(verbose_name='Descrizione oggetto breve', max_length=256)
+
+    #stato di conservazione
+
+    motivo = models.TextField(verbose_name='Motivo per la fruizione', max_length=256)
+
+    #riferimenti bibiliografici
+    #riferimenti sitografici
+    #rilevatore (utente)
+
+    data = models.DateField(verbose_name='Data inserimento', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Punto di Interesse'
+        verbose_name_plural = 'Punti di Interesse'
+
+    def __str__(self):
+        return self.nome
+
+
 class TipoInteresse(models.Model):
     descrizione = models.CharField(verbose_name='Descrizione', max_length=128, unique=True)
 
@@ -13,7 +55,7 @@ class TipoInteresse(models.Model):
 
 class InteresseSpecifico(models.Model):
     descrizione = models.CharField(verbose_name='Descrizione', max_length=128, unique=True)
-    tipo = models.ForeignKey(TipoInteresse, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(TipoInteresse, verbose_name='Tipo', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Interesse Specifico'
