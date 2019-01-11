7115f04b4ae4ae2059422d8314c1b9776b54d65b
Gianpaolo Branca <gianpaolo@protonmail.com>
2019-01-11 15:19:31 +0100

    Create pages for 'rilevatore' and 'validatore'

--- punti_interesse/urls.py
+++ punti_interesse/urls.py
@@ -3,4 +3,6 @@ from punti_interesse import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^ril/', views.rilevatore, name='rilevatore'),
+    url(r'^val/', views.validatore, name='validatore'),
 ]
--- punti_interesse/views.py
+++ punti_interesse/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'punti_interesse/index.html')
+
+def rilevatore(request):
+    return render(request, 'punti_interesse/rilevatore.html')
+
+def validatore(request):
+    return render(request, 'punti_interesse/validatore.html')
