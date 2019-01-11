3c70cb9330b3dc8555985d22a25c7bc6568fb46e
Gianpaolo Branca <gianpaolo@protonmail.com>
2019-01-11 18:09:30 +0100

    Create page for point of interest

--- punti_interesse/urls.py
+++ punti_interesse/urls.py
@@ -3,6 +3,7 @@ from punti_interesse import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^ril/', views.rilevatore, name='rilevatore'),
-    url(r'^val/', views.validatore, name='validatore'),
+    url(r'^ril/$', views.rilevatore, name='rilevatore'),
+    url(r'^val/$', views.validatore, name='validatore'),
+    url(r'^ril/(?P<pi_name_slug>[\w\-]+)/$', views.show_pi_ril, name='show_pi_ril'),
 ]
--- punti_interesse/views.py
+++ punti_interesse/views.py
@@ -11,3 +11,12 @@ def rilevatore(request):
 def validatore(request):
     lista_punti = PuntoInteresse.objects.order_by('data')
     return render(request, 'punti_interesse/validatore.html', {'punti': lista_punti})
+
+def show_pi_ril(request, pi_name_slug):
+    context_dict = {}
+    try:
+        punto_interesse = PuntoInteresse.objects.get(slug=pi_name_slug)
+        context_dict['punto'] = punto_interesse
+    except PuntoInteresse.DoesNotExist:
+        context_dict['punto'] = None
+    return render(request, 'punti_interesse/pi.html', context_dict)
