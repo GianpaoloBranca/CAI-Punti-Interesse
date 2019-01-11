ea89709478d264634cbd6b95d3f2356fd0ab67c8
Gianpaolo Branca <gianpaolo@protonmail.com>
2019-01-11 16:22:11 +0100

    Add table in rilevatore.html

--- punti_interesse/views.py
+++ punti_interesse/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from punti_interesse.models import PuntoInteresse
 
 def index(request):
     return render(request, 'punti_interesse/index.html')
 
 def rilevatore(request):
-    return render(request, 'punti_interesse/rilevatore.html')
+    lista_punti = PuntoInteresse.objects.order_by('data')
+    return render(request, 'punti_interesse/rilevatore.html', {'punti': lista_punti})
 
 def validatore(request):
-    return render(request, 'punti_interesse/validatore.html')
+    lista_punti = PuntoInteresse.objects.order_by('data')
+    return render(request, 'punti_interesse/validatore.html', {'punti': lista_punti})
