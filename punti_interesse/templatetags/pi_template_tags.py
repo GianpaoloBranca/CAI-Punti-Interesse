6a626af4e1c0dbbb3fa7ab71671dc9051bd6ce9d
DESKTOP-35ND6R8\gianpaolo <gianpaolo.branca@protonmail.com>
2019-01-31 17:40:03 +0100

    Add inclusion tag with button to scroll to the top

--- punti_interesse/templatetags/pi_template_tags.py
+++ punti_interesse/templatetags/pi_template_tags.py
@@ -9,3 +9,7 @@ def is_rilevatore(user):
 @register.filter
 def is_validatore(user):
     return user.groups.filter(name='Validatore').exists()
+
+@register.inclusion_tag('top_scroll.html')
+def btn_scroll_top():
+    return {}
