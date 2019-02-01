1298352102ead03b4da49dade4b534b795dbeaf1
Gianpaolo Branca <gianpaolo@protonmail.com>
2019-02-01 11:50:42 +0100

    Improve form buttons style

--- punti_interesse/templatetags/pi_template_tags.py
+++ punti_interesse/templatetags/pi_template_tags.py
@@ -10,6 +10,10 @@ def is_rilevatore(user):
 def is_validatore(user):
     return user.groups.filter(name='Validatore').exists()
 
+@register.inclusion_tag('punti_interesse/form_buttons.html', takes_context=True)
+def form_buttons(context):
+    return {'punto': context.get('punto', None)}
+
 @register.inclusion_tag('top_scroll.html')
 def btn_scroll_top():
     return {}
